652904f22af44a2a7b7f86390f5e902e3ee35b91
Samantha Berk <sberk10@gmail.com>
2018-05-21 15:20:04 -0700

    sound plays on click

--- noise.js
+++ noise.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
-  // your code here
+  $('.c').click( function() {
+    $('audio#cAudio')[0].play();
+  });
+  $('.d').click( function() {
+    $('audio#dAudio')[0].play();
+  });
+  $('.e').click( function() {
+    $('audio#eAudio')[0].play();
+  });
+  $('.f').click( function() {
+    $('audio#fAudio')[0].play();
+  });
+  $('.g').click( function() {
+    $('audio#gAudio')[0].play();
+  });
+  $('.a').click( function() {
+    $('audio#aAudio')[0].play();
+  });
+  $('.b').click( function() {
+    $('audio#bAudio')[0].play();
+  });
 });
