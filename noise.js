c0e6cdd9502a57a01b17139d48f093346ed991d0
Samantha Berk <sberk10@gmail.com>
2018-05-21 15:36:37 -0700

    sounds play on key press

--- noise.js
+++ noise.js
@@ -1,3 +1,4 @@
+// Trigger correct note when someone clicks inside a box
 $(document).ready( function() {
   $('.c').click( function() {
     $('audio#cAudio')[0].play();
@@ -19,5 +20,25 @@ $(document).ready( function() {
   });
   $('.b').click( function() {
     $('audio#bAudio')[0].play();
+  });
+});
+
+$(document).ready( function() {
+  $('body').keypress((event) => {
+    if ( event.key === 'a' ) {
+      $('audio#aAudio')[0].play();
+    } else if ( event.key === 'b' ) {
+      $('audio#bAudio')[0].play();
+    } else if ( event.key === 'c' ) {
+      $('audio#cAudio')[0].play();
+    } else if ( event.key === 'd' ) {
+      $('audio#dAudio')[0].play();
+    } else if ( event.key === 'e' ) {
+      $('audio#eAudio')[0].play();
+    } else if ( event.key === 'f' ) {
+      $('audio#fAudio')[0].play();
+    } else if ( event.key === 'g' ) {
+      $('audio#gAudio')[0].play();
+    }
   });
 });
